123b94b7a590b875d67f17f02e00849e7e813293
isaacmg <igodfried@isaac26.com>
2019-02-26 18:16:36 -0500

    cast to int too

--- models/Mem2Seq.py
+++ models/Mem2Seq.py
@@ -131,8 +131,8 @@ class Mem2Seq(nn.Module):
                 all_decoder_outputs_ptr[t] = decoder_ptr
                 ## get the correspective word in input
                 top_ptr_i = torch.gather(input_batches[:,:,0],0,Variable(toppi.view(1, -1)))
-                # .item()? is it right here
-                next_in = [top_ptr_i.squeeze()[i].item() if(toppi.squeeze()[i].item() < input_lengths[i]-1) else topvi.squeeze()[i].item() for i in range(batch_size)]
+                # .item()? is it right here. Also added casting to an int
+                next_in = [top_ptr_i.squeeze()[i].item() if(int(toppi.squeeze()[i].item()) < input_lengths[i]-1) else topvi.squeeze()[i].item() for i in range(batch_size)]
                 decoder_input = Variable(torch.LongTensor(next_in)) # Chosen word is next input
                 if USE_CUDA: decoder_input = decoder_input.cuda()
                   
